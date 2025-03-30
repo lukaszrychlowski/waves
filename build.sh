527b8ae5510ba7371444c164d162d695029c6afd
user <rychlowski.lukasz@gmail.com>
2025-03-30 13:40:07 +0200

    moar warnings

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-gcc src/main.c src/callbacks.c src/window.c src/renderer.c  src/gl.c -Iinclude -Llib -lglfw3 -lm -o main
+gcc src/main.c src/callbacks.c src/window.c src/renderer.c  src/gl.c -Iinclude -Llib -lglfw3 -lm -o main -Wall -Wextra -Wpedantic -Wcast-align
